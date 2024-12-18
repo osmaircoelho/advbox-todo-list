342ff716a3aa79dfe00b9e5a247553012bc833dd
Osmair Coelho <osmair.coelho@gmail.com>
2024-12-18 11:07:53 -0300

    update planejamento tarefa e dashboard nao foi adicionado o loading

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,13 +10,13 @@
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 bg-white border-b border-gray-200">
-                        @livewire('task-manager')
+                        <livewire:task-manager/>
                     </div>
                 </div>
 
                 <div class="mt-8 bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 bg-white border-b border-gray-200">
-                        @livewire('category-manager')
+                        <livewire:category-manager/>
                     </div>
                 </div>
             </div>
